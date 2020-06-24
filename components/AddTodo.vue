0750bdfb042ed87c302cee7bbb668fe25e4bd85d
Nishino0719 <nishino0719@icloud.com>
2020-06-24 17:54:40 +0900

    deadlineを秒数指定でデータベースに保存、レベル分けできるようになりました。

--- components/AddTodo.vue
+++ components/AddTodo.vue
@@ -37,13 +37,13 @@
                 <el-form-item label="課題締め切り" required>
                     <el-col :span="11">
                     <el-form-item prop="date1">
-                        <el-date-picker type="date" placeholder="日付を選んでね！" v-model="ruleForm.date1" style="width: 100%;"></el-date-picker>
+                        <el-date-picker type="date" placeholder="日付を選んでね！" v-model="ruleForm.date1" :picker-options="pickerOptions" style="width: 100%;"></el-date-picker>
                     </el-form-item>
                     </el-col>
                     <el-col class="line" :span="1">ー</el-col>
                     <el-col :span="11">
                     <el-form-item prop="time">
-                          <el-time-select v-model="ruleForm.time" :picker-options="{ start: '0:00', step: '00:15', end: '24:00'}" placeholder="時間を選んでね！" style="width:100%;"></el-time-select>
+                          <el-time-select v-model="ruleForm.time" minTime:nowTime :picker-options="{ start: '0:00', step: '00:15', end: '24:00'}" placeholder="時間を選んでね！" style="width:100%;"></el-time-select>
                     </el-form-item>
                     </el-col>
                 </el-form-item>
@@ -61,13 +61,48 @@
 
 <script>
 import {db} from '~/plugins/firebase'
+import moment from 'moment'
+moment.locale('ja')
 export default {
     data(){
         return {
+            nowTime:'',
             visible:false,
             drawer:false,
             tags:[],
             level:1,
+              pickerOptions: {
+                disabledDate(time) {
+                  return time.getTime() < Date.now();
+                },
+                shortcuts: [{
+                  text: '今日',
+                  onClick(picker) {
+                    picker.$emit('pick', new Date());
+                  }
+                }, {
+                  text: '明日',
+                  onClick(picker) {
+                    const date = new Date();
+                    date.setTime(date.getTime() + 3600 * 1000 * 24);
+                    picker.$emit('pick', date);
+                  }
+                },  {
+                  text: '三日後',
+                  onClick(picker) {
+                    const date = new Date();
+                    date.setTime(date.getTime() + 3600 * 1000 * 24 * 3);
+                    picker.$emit('pick', date);
+                  }
+                }, {
+                  text: '1週間後',
+                  onClick(picker) {
+                    const date = new Date();
+                    date.setTime(date.getTime() + 3600 * 1000 * 24 * 7);
+                    picker.$emit('pick', date);
+                  }
+                }]
+              },
             ruleForm: {
                 tag: '',
                 date1: '',
@@ -115,20 +150,20 @@ export default {
                 this.$message({
                 type: 'warning',
                 message: 'ごめんなさい！タグは9文字以内でお願いします！'
-              });
+              })
               this.tagForm.tag = ''
               this.ruleForm.tag = ''
         }else if(this.tagForm.tag == ''){
               this.$message({
                 type: 'warning',
                 message: 'タグを入力してね！'
-              });
+              })
               this.ruleForm.tag = ''
         }else if(this.tagForm.color == ''){
               this.$message({
                 type: 'warning',
                 message: '色を入力してね！'
-              });
+              })
               this.ruleForm.tag = ''
         }
       },
@@ -136,17 +171,41 @@ export default {
         this.$refs[formName].validate((valid) => {
           if(valid){
             // date1のformatを日付までにして、timeのformatを時間指定してつなぎ合わせたものをdeadlineとして渡せていない
+                const  start_time = moment("0:00", 'HH:mm')
+                const  end_time = moment(this.ruleForm.time, 'HH:mm')
+                const deadlineSecond = moment(this.ruleForm.date1).startOf('day').unix() + end_time.diff(start_time)/1000
+                const date = new Date() ;
+                const milli = date.getTime()
+                const nowSecond =  Math.floor(milli/1000)
+                const timeDiff = deadlineSecond - nowSecond
+                // console.log('msで出てる？→', end_time.diff(start_time)/1000)
+                // console.log('format変えてる', moment(this.ruleForm.date1).format('l'))
+                // console.log('startOf',moment(this.ruleForm.date1).startOf('day'))
+                // console.log('startOf.unix',moment(this.ruleForm.date1).startOf('day').unix())
+                // console.log('deadlineの秒数はこっちになります！',moment(this.ruleForm.date1).startOf('day').unix() + end_time.diff(start_time)/1000)
+                // console.log('今の秒数',nowSecond)
+                // console.log('残り',timeDiff)
+
+                if(timeDiff <= 60 * 60 * 24 * 1){
+                  this.level = 4
+                }else if(timeDiff <= 60 * 60 * 24 * 3){
+                  this.level = 3
+                }else if(timeDiff <= 60 * 60 * 24 * 7){
+                  this.level = 2
+                }else{
+                  this.level = 1
+                }
+            this.drawer = false
             const channelId = this.$route.params.id
-            if(this.ruleForm.tag !== 'タグを追加'){
+            if(this.ruleForm.tag !== 'タグを追加' && timeDiff > 0){
               db.collection('channels').doc(channelId).collection('tasks').add({
                 text: this.ruleForm.text,
                 tag: this.ruleForm.tag,
-                deadline: this.ruleForm.date1,
+                deadline: deadlineSecond,
                 level:this.level,
                 done:false,
               })
             }
-            this.drawer = false
           }else{
             return false;
           }
@@ -157,7 +216,14 @@ export default {
       },
       addTask(formName) {
         //   成功した時のみにしなきゃ
-        if( this.ruleForm.tag !== '' && this.ruleForm.date1 !== '' && this.ruleForm.date2 !== '' &&this.ruleForm.tag !== 'タグを追加'){
+                const  start_time = moment("0:00", 'HH:mm')
+                const  end_time = moment(this.ruleForm.time, 'HH:mm')
+                const deadlineSecond = moment(this.ruleForm.date1).startOf('day').unix() + end_time.diff(start_time)/1000
+                const date = new Date() ;
+                const milli = date.getTime()
+                const nowSecond =  Math.floor(milli/1000)
+                const timeDiff = deadlineSecond - nowSecond
+        if( this.ruleForm.tag !== '' && this.ruleForm.date1 !== '' && this.ruleForm.date2 !== '' &&this.ruleForm.tag !== 'タグを追加' &&  timeDiff > 0){
           this.$notify({
             title: '成功！',
             message: 'タスクが新たに追加されました！',
@@ -166,7 +232,22 @@ export default {
         }else{
           console.log('errorですよ')
           if(this.ruleForm.tag === 'タグを追加'){
-            alert('「タグを追加」はタグとして使うことができません。')
+            // alert('「タグを追加」はタグとして使うことができません。')
+            this.$message({
+                type: 'error',
+                duration: 5000,
+                showClose: true,
+                message: '「タグを追加」はタグとして使うことができません。'
+              });
+          }
+          if( timeDiff <= 0){
+            // alert('Oops!! 今の時間以降を入力してね！')
+                this.$message({
+                type: 'error',
+                duration: 5000,
+                showClose: true,
+                message: 'この時間は選べないよ！今の時間以降を選んでね！'
+              });
           }
         }
       },
@@ -180,7 +261,7 @@ export default {
             this.tags.push({id: doc.id, ...doc.data()})
           }
         })
-      })
+      })       
     }
 }
 </script>
